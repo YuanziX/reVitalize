2db148a387e12ba4a8108240822ffb6ae53c3d2f
YuanziX <achubadyal4@gmail.com>
2024-07-30 20:52:42 +0530

    bottomNavBar: change icons

--- app/src/main/java/dev/yuanzix/revitalize/navigation/BottomNavBarItems.kt
+++ app/src/main/java/dev/yuanzix/revitalize/navigation/BottomNavBarItems.kt
@@ -1,12 +1,30 @@
 package dev.yuanzix.revitalize.navigation
 
+import androidx.annotation.DrawableRes
 import dev.yuanzix.revitalize.R
 
-open class NavItem(val screen: Screen, val title: String, val icon: Int)
+open class NavItem(val screen: Screen, val title: String, @DrawableRes val iconId: Int)
 
 object NavRoutes {
-    val Home = NavItem(Screen.HomeScreen, NavPath.Home.toString(), R.drawable.outline_home_app_logo)
-    val Attendance = NavItem(Screen.AttendanceScreen, NavPath.Attendance.toString(), R.drawable.rounded_calendar_month)
-    val Performance = NavItem(Screen.PerformanceScreen, NavPath.Performance.toString(), R.drawable.rounded_numbers)
-    val Others = NavItem(Screen.OthersScreen, NavPath.Others.toString(), R.drawable.rounded_more_horiz)
+    val Home = NavItem(
+        screen = Screen.HomeScreen,
+        title = NavPath.Home.toString(),
+        iconId = R.drawable.home
+    )
+    val Attendance = NavItem(
+        screen = Screen.AttendanceScreen,
+        title = NavPath.Attendance.toString(),
+        iconId = R.drawable.attendance
+    )
+    val Performance = NavItem(
+        screen = Screen.PerformanceScreen,
+        title = NavPath.Performance.toString(),
+        iconId = R.drawable.performance
+    )
+    val Others =
+        NavItem(
+            screen = Screen.OthersScreen,
+            title = NavPath.Others.toString(),
+            iconId = R.drawable.more
+        )
 }
--- app/src/main/java/dev/yuanzix/revitalize/navigation/BottomNavigationBar.kt
+++ app/src/main/java/dev/yuanzix/revitalize/navigation/BottomNavigationBar.kt
@@ -1,6 +1,8 @@
 package dev.yuanzix.revitalize.navigation
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
@@ -9,21 +11,31 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun BottomNavigationBar(
-    navController: NavController
+    navController: NavController,
 ) {
-    val navItems = listOf(NavRoutes.Home, NavRoutes.Attendance, NavRoutes.Performance, NavRoutes.Others)
+    val navItems =
+        listOf(NavRoutes.Home, NavRoutes.Attendance, NavRoutes.Performance, NavRoutes.Others)
     var selectedItem by rememberSaveable { mutableIntStateOf(0) }
 
     NavigationBar {
         navItems.forEachIndexed { index, item ->
             NavigationBarItem(
                 alwaysShowLabel = true,
-                icon = { Icon(painterResource(item.icon), contentDescription = item.title) },
+                icon = {
+                    Icon(
+                        modifier = Modifier.size(20.dp),
+                        painter = painterResource(item.iconId),
+                        contentDescription = item.title,
+                        tint = MaterialTheme.colorScheme.primary,
+                    )
+                },
                 label = { Text(item.title) },
                 selected = selectedItem == index,
                 onClick = {
@@ -33,5 +45,4 @@ fun BottomNavigationBar(
             )
         }
     }
-
 }
